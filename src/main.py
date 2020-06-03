58c4ab0d12bb72659aabdffcb53a040190af4189
poymanov <n.poymanov@gmail.com>
2020-06-03 18:49:00 +0300

    Логика приложения перенесена в отдельный метод

--- src/main.py
+++ src/main.py
@@ -11,52 +11,52 @@ TELEGRAM_USER_CHAT_ID = os.environ['TELEGRAM_USER_CHAT_ID']
 
 
 def init_telegram_bot():
-    if not TELEGRAM_SOCKS5_PROXY:
-        proxy_request = None
-    else:
+    proxy_request = None
+
+    if TELEGRAM_SOCKS5_PROXY:
         proxy_request = telegram.utils.request.Request(proxy_url='socks5://{}'.format(TELEGRAM_SOCKS5_PROXY))
 
     return telegram.Bot(token=TELEGRAM_BOT_TOKEN, request=proxy_request)
 
 
-def send_message(bot, message):
-    bot.send_message(chat_id=TELEGRAM_USER_CHAT_ID, text=message)
+def main():
+    query_timestamp = None
 
+    bot = init_telegram_bot()
 
-query_timestamp = None
+    while True:
+        try:
+            headers = {'Authorization': 'Token {}'.format(DEVMAN_AUTH_TOKEN)}
+            params = {}
 
-bot = init_telegram_bot()
+            if query_timestamp is not None:
+                params['timestamp'] = query_timestamp
 
-while True:
-    try:
-        headers = {'Authorization': 'Token {}'.format(DEVMAN_AUTH_TOKEN)}
-        params = {}
+            response = requests.get(DEVMAN_API_URL, headers=headers, params=params)
+            response_data = response.json()
 
-        if query_timestamp is not None:
-            params['timestamp'] = query_timestamp
+            response_data_status = response_data['status']
 
-        response = requests.get(DEVMAN_API_URL, headers=headers, params=params)
-        response_data = response.json()
+            if response_data_status == 'timeout':
+                query_timestamp = response_data['timestamp_to_request']
+            else:
+                if response_data_status == 'found':
+                    attempt_data = response_data['new_attempts'][0]
+                    lesson_title = attempt_data['lesson_title']
+                    lesson_negative = attempt_data['is_negative']
 
-        response_data_status = response_data['status']
+                    if lesson_negative is True:
+                        lesson_result_message = 'К сожалению, в работе нашлись ошибки.'
+                    else:
+                        lesson_result_message = 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
 
-        if response_data_status == 'timeout':
-            query_timestamp = response_data['timestamp_to_request']
-        else:
-            if response_data_status == 'found':
-                attempt_data = response_data['new_attempts'][0]
-                lesson_title = attempt_data['lesson_title']
-                lesson_negative = attempt_data['is_negative']
+                    message = 'У вас проверили работу "{}".\n\n{}'.format(lesson_title, lesson_result_message)
+                    bot.send_message(chat_id=TELEGRAM_USER_CHAT_ID, text=message)
 
-                if lesson_negative is True:
-                    lesson_result_message = 'К сожалению, в работе нашлись ошибки.'
-                else:
-                    lesson_result_message = 'Преподавателю всё понравилось, можно приступать к следующему уроку!'
+                query_timestamp = None
+        except (requests.exceptions.ReadTimeout, ConnectionError):
+            time.sleep(5)
+            continue
 
-                message = 'У вас проверили работу "{}".\n\n{}'.format(lesson_title, lesson_result_message)
-                send_message(bot, message)
-
-            query_timestamp = None
-    except (requests.exceptions.ReadTimeout, ConnectionError):
-        time.sleep(5)
-        continue
+if __name__ == '__main__':
+    main()
